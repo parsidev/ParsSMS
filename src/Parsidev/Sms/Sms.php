d4d818c1c9947b9b24108cd65ad19d8b8012875a
Mohammad Reza <parsidev@users.noreply.github.com>
2015-03-06 13:52:10 +0330

    Update Sms.php

--- src/Parsidev/Sms/Sms.php
+++ src/Parsidev/Sms/Sms.php
@@ -1,11 +1,9 @@
 <?php namespace Parsidev\Sms;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\Config;
 use SoapClient;
 
->>>>>>> origin/master
+
 class Sms {
     
     
@@ -13,24 +11,12 @@ class Sms {
         protected $client;
     
     public function __construct($config, $client) {
-<<<<<<< HEAD
         $this->confg = $config;
         $this->client = $client;
-=======
-        /*if(!self::$singleton){
-            $instance = new self();
-            $instance->config = config('sms');
-            self::$singleton = $instance;
-        }
-        return self::$singleton;*/
-        $this->confg = $config;
-        $this->client = $client;
-        
->>>>>>> origin/master
     }
     
     public function send($to, $text, $isFlash=FALSE){
-        $param = self::$singleton->config;
+        $param = $this->confg;
         
         !is_array($to) ? $to = array($to) : false;
         
@@ -44,14 +30,6 @@ class Sms {
     
     
     public function getCredit(){
-<<<<<<< HEAD
         return ($this->client->GetCredit($this->confg)->GetCreditResult);
-=======
-        dd($this->confg);
-        $client = new SoapClient(self::$singleton->config['webserviceUrl']);
-        
-        return ($client->GetCredit(self::$singleton->config)->GetCreditResult);
->>>>>>> origin/master
-        
     }
 }
